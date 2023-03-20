791d9b653ee84fcd449aa43d24fd564396136c4f
jessemeekins <jessemeekins@me.com>
2023-03-20 18:34:37 -0500

    Cleaned up Structures

--- app/StructureTemplate.py
+++ app/StructureTemplate.py
@@ -8,29 +8,23 @@ log = logging.Logger('.//default_logger.log')
 class DataUploaderClass(ABC):
     @abstractmethod
     def file_importer(self):
-        """retrieve file based on file type"""
+        """file importer"""
 
-    @abstractmethod
-    def load_data(self):
-        """upload file into program ready for logic"""
 
 
 class XMLFileUploader(DataUploaderClass):
     def file_importer(self) -> str:
         file_name = Formatters.XMLFormatterClass.XML_file_name()
-        return file_name
-        
-    def load_data(self):
-        return Formatters.XMLFormatterClass.XML_file_data()
- 
 
-class XLSXFileUploader(DataUploaderClass):
+        return file_name, Formatters.XMLFormatterClass.XML_file_data() 
+
+
+class ParTelestaffFileUploader(DataUploaderClass):
     def file_importer(self) -> str:
+        
         file_name = Formatters.XLSXFormatterClass.XLSX_importer()
-        return file_name
+        return file_name, '[*]'
     
-    def load_data(self):
-        ...
 
 class PatternClass(ABC):
     def __init__(self, uploader: DataUploaderClass) -> None:
@@ -40,65 +34,92 @@ class PatternClass(ABC):
         return f"{self.uploader}"
         
     @abstractmethod
-    def function_1(self):
+    def class_function(self, *args):
         """Initilized method_1"""
-
-    @abstractmethod
-    def function_2(self):
-        """Initilized method_2"""
 
     def execute(self): ...
 
-
-class PatternClassMethod_1(PatternClass):
-    def function_1(self, arg: dict) -> list:
+class ALSCountClassMethod(PatternClass):
+    def class_function(self, arg: dict) -> list:
         if arg:
             ALS_record = list(map(lambda x: x["company"] ,filter(lambda x: x["position"] =='1.1', arg.values())))
-            print("[*] PatternClassMethod_1::function_1 executed")
-            return ALS_record
-        else: ...
-     
-    def function_2(self, arg: list) -> None: 
-        if arg:
-            print("[*] Recieved List")
-            l = len(list(set(arg)))
-            filtered_list = list(filter(lambda x: "BC" not in x, filter(lambda x: "EU" not in x, arg)))
-            print("[*] Count:" , len(list(filtered_list)) )
-            print(sorted(filtered_list))
-
-        else: ... 
-
-class PatternClassMethod_2(PatternClass):
-    def function_1(self, arg: None) -> None:
-        if arg:
-            print("[*] PatternClassMethod_2::function_1 executed")
-        else: ...
-    def function_2(self, arg: None) -> None:
-        if arg:
-            print("[*] PatternClassMethod_2::function_2 executed")
+            records = list(filter(lambda x: "BC" not in x, filter(lambda x: "EU" not in x, ALS_record)))
+            print("[*] Count:" , len(set(list(records))))
+            return records
         else: ...
 
+
+class RosterFilterClass(ABC):
+    def __init__(self, uploader: DataUploaderClass) -> None:
+        self.uploader = uploader
+
+    @abstractmethod
+    def class_function(self, arg: dict, *args, **kwargs):
+        """Filtered mon rank"""
+
+        if args:
+            try:
+                records = list(map(lambda x: (x["company"], x["name"], x["paycode"]) ,filter(lambda x: x["rank"] == args[0], arg.values())))
+                return records
+            except:
+                pass
+        else:
+            ...
+
+
+class LieutenantClassMethod(RosterFilterClass):
+    def class_function(self, arg: dict):
+        return super().class_function(arg, 'LT')
+
+class BattalionChiefClassMethod(RosterFilterClass):
+    def class_function(self, arg: dict):
+        return super().class_function(arg, 'BC')
+
+class DivisionChiefClassMethod(RosterFilterClass):
+    def class_function(self, arg: dict):
+        return super().class_function(arg, 'DC')
+
+      
+
+    
+class OvertimeCallBackMethod(PatternClass):
+    def class_function(self, arg: dict) -> list:
+        if arg:
+            now = Formatters.DatetimeTimeFormatter.local_timestamp()
+            record = list(map(lambda x: (x["name"], x["paycode"]) ,filter(lambda x: x["end"] > now, filter(lambda x: x["paycode"] =='OTCB', arg.values()))))
+            #records = list(filter(lambda x: "BC" not in x, filter(lambda x: "EU" not in x, record)))
+            print("[*] Count:" , len(set(list(record))))
+            return record
+        else: ...
+
+# Main function and logic of the class
 
 def execute(method:str) -> PatternClass:
     registered_methods = {
-        "one": PatternClassMethod_1(XMLFileUploader()),
-        "two": PatternClassMethod_2(XLSXFileUploader())
+        "als": ALSCountClassMethod(XMLFileUploader()),
+        "lt": LieutenantClassMethod(XMLFileUploader()),
+        "bc": BattalionChiefClassMethod(XMLFileUploader()),
+        "dc": DivisionChiefClassMethod(XMLFileUploader()),
+        "ot": OvertimeCallBackMethod(XMLFileUploader()),
     }
     choice = registered_methods.get(method, None)
     return choice
 
 
-def main(method:str) -> None:
+def main(method:str, verbose=False) -> None:
+    """Main function to handle the Pattern Stratigy (Bridge patter included)"""
     class_pattern = execute(method=method)
-    file = class_pattern.uploader.file_importer()
-    source = class_pattern.uploader.load_data()
-    parsed_data = class_pattern.function_1(source)
-    data = class_pattern.function_2(parsed_data)
+    file , data = class_pattern.uploader.file_importer()
+    parsed_data = class_pattern.class_function(data)
     complete_time = Formatters.DatetimeTimeFormatter.local_timestamp()
+    print(f'[*] {file}')
     print(f"[{complete_time}] Execution completed on file name :: {file}")
+    if verbose:
+        print(parsed_data)
 
 
 if __name__ == "__main__":
-    choice = input("Method choice ('one', 'two')?")
-    main(method=choice)
+    choice = input("Method choice ('ALS', 'two')?")
+    main(method=choice.lower(), verbose=True)
+
 

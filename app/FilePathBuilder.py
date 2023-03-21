6966a53e398c4066fe4a3af15b6851bef90d5ac4
jessemeekins <jessemeekins@me.com>
2023-03-20 22:53:48 -0500

    Data restructured

--- app/FilePathBuilder.py
+++ app/FilePathBuilder.py
@@ -1,48 +1,46 @@
 #%%
-
-
 class FilePathBuilder:
 
     configuration = {
-        "PAR" : ('local', 'ROS11'),
-        "STAFF": ('network', "ROS12"),
-        "WEB": ('web', "ROS13"),
-        "ALS": ("local", "ROS12")
+        "PAR" : {"path":'local', "file":'ROS11', "debug": True},
+        "STAFF": {"path":'network', "file":'ROS12', "debug": False},
+        "WEB": {"path":'web', "file":'ROS13'},
+        "ALS": {"path":'local', "file":'ROS12'}
     }
     file_source = {
         'local': '/Users/jessemeekins/Documents/XML_EXPORTS/',
         'network': '/usr/bin/files/exports/',
-        'web': 'https://192.168.1.244:5000'    
+        'web': 'https://192.168.1.244:5000/'    
     }
     file_name = {
         'ROS11': 'ROS11 MFD2023-10-02.xml',
         'ROS12': 'ROS12 MFD2023-10-11.xml',
         'ROS13': 'ROS13 MFD2023-10-11.xml'
     }
-    def __init__(self, config: tuple) -> None:
+
+    def __init__(self, config: str) -> None:
         self.config = config
-        
-    def file_path_formatter(self) -> str:
+            
+    def get_filepath(self) -> str:
         conf = self.configuration.get(self.config, '')
-        path = self.file_source.get(conf[0], '')
-        name = self.file_name.get(conf[1], '')
-        
+        path = self.file_source.get(conf["path"], '')
+        name = self.file_name.get(conf["file"], '')
         return f"{path}{name}"
+    
+    def debug(self) -> bool:
+        conf = self.configuration.get(self.config, '')
+        debug = conf.get("debug", True)
+        return debug
   
+
+class TelestaffFilePathBuilder(FilePathBuilder):
+    def __init__(self, config: str) -> None:
+        super().__init__(config)
     
 
-class TelestaffFilePathFormatter(FilePathBuilder):
-    def __init__(self, config) -> None:
-        super().__init__(config)
 
-report1 = TelestaffFilePathFormatter("PAR").file_path_formatter()
-report2 = TelestaffFilePathFormatter("STAFF").file_path_formatter()
-report3 = TelestaffFilePathFormatter("ALS").file_path_formatter()
 
-print(report1)
-print(report2)
-print(report3)
-        
+
 
 
     

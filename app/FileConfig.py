60fa6d2c64fed3ef1e97b42cb8f0bf798b3788ec
jessemeekins <jessemeekins@me.com>
2023-03-25 20:43:49 -0500

    Updated to simple config class

---
+++ app/FileConfig.py
@@ -0,0 +1,29 @@
+#%%
+
+from app.secrets import *
+from dataclasses import dataclass
+
+from datetime import datetime, timedelta
+today = datetime.today() - timedelta(hours=7)
+formatted_time = datetime.strftime(today, "%Y-%m-%d")
+
+@dataclass
+class FileConfig:
+
+    configuration = {
+        "PAR" : {"port": PORT, "username": SFTP_USERNAME, "password": SFTP_PASSWORD,"location":IP_ADDRESS, "extension":FILE_EXTENSION, "destination": LOCAL_PATH, "filename": f'ROS11 MFD{formatted_time}.xml', "debug": True},
+    }
+
+    def __init__(self, config: str) -> None:
+        self.config = config
+            
+    def get_file_data(self) -> dict:
+        config = self.configuration.get(self.config, '')
+        return config
+
+    
+FileConfig("PAR")
+
+
+
+    

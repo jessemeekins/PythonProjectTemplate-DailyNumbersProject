2b07703a99a89bb768d2bb0e992d5334c4e89a79
jessemeekins <jessemeekins@me.com>
2023-04-07 08:45:43 -0500

    os enviro intial commit

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
+from SFTPClient._secrets import Secrets
+
+import os
 import setuptools
 
 setuptools.setup(setup_requires=['pbr'], pbr=True)
+
+os.environ["SFTP_PORT"] = Secrets.PORT
+os.environ["SFTP_SERVER"] = Secrets.IP_ADDRESS
+os.environ["PROJECT_PATH"] =Secrets.PROJECT_PATH
+os.environ["SFTP_USERNAME"] = Secrets.SFTP_USERNAME
+os.environ["SFTP_PASSWORD"] = Secrets.SFTP_PASSWORD
+

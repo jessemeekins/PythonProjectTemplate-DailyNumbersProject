49da28dca02137954fcc07341d0d4aef65eaf025
jessemeekins <jessemeekins@me.com>
2023-04-07 08:45:20 -0500

    MIT License added

--- app/FileConfig.py
+++ app/FileConfig.py
@@ -1,6 +1,27 @@
 #%%
 
-from SFTPClient._secrets import Secrets
+"""
+Copyright (c) 2023 Jesse Meekins
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+import os
 from dataclasses import dataclass
 from datetime import datetime, timedelta
 
@@ -15,12 +36,12 @@ formatted_time = adjusted_time()
 class FileConfig:
 
     configuration = {
-        "PAR" : {"port": Secrets.PORT, 
-        "username": Secrets.SFTP_USERNAME, 
-        "password": Secrets.SFTP_PASSWORD,
-        "location":Secrets.IP_ADDRESS, 
-        "extension":Secrets.FILE_EXTENSION, 
-        "destination": Secrets.LOCAL_PATH, 
+        "PAR" : {"port": os.environ["SFTP_PORT"], 
+        "username": os.environ["SFTP_USERNAME"], 
+        "password": os.environ["SFTP_PASSWORD"],
+        "location": os.environ["IP_ADDRESS"], 
+        "extension": os.environ["FILE_EXTENSION"], 
+        "destination": os.environ["LOCAL_PATH"], 
         "filename": f'ROS11 MFD{formatted_time}.xml', 
         "debug": True},
     }

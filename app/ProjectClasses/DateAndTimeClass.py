14e5390f18c02385aa53af9c84c802bc05c04c02
jessemeekins <jessemeekins@me.com>
2023-04-12 07:53:10 -0500

    removed debug print

--- app/ProjectClasses/DateAndTimeClass.py
+++ app/ProjectClasses/DateAndTimeClass.py
@@ -36,5 +36,4 @@ class DateTimeFormatter:
     def shift_start_end_adjust() -> str:
         today = datetime.today() - timedelta(hours=7)
         formatted_time = datetime.strftime(today, "%Y-%m-%d")
-        print(formatted_time)
         return formatted_time 

0ff0d6e73389a6370107cb7b5c4f0d4a9ce7a4ea
jessemeekins <jessemeekins@me.com>
2023-04-12 07:52:52 -0500

    Added typehint for mapper and doc str's

--- app/ProjectClasses/DataParser.py
+++ app/ProjectClasses/DataParser.py
@@ -20,15 +20,22 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+from mapper import DataFieldsMapper
+
 class DataParser:
-    def __init__(self, data, mapper:object, key_tag='Record') -> None:
+    """Loops though each record of records in a XML file, 'key_tag' set to record,
+    the internal file tag for each individual record in telestaff in a given report"""
+    def __init__(self, data, mapper: DataFieldsMapper, key_tag='Record') -> None:
         self.data = data
-        self.mapper = mapper
+        self.mapper: DataFieldsMapper = mapper
         self.key = key_tag
 
+
     def xml_into_dictionary(self) -> dict:
+        """Takes in the XML.Tree from the XMLImporter Class and loops through the records"""
         records_dict = dict()
         for i, child in enumerate(self.data.iter(self.key)):
             record = self.mapper.field_mapper(child)
             records_dict[i] = record
         return records_dict
+

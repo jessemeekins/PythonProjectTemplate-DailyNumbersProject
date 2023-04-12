ccb5cfa6a767cf0c4c8d3df56ee52040a910c665
jessemeekins <jessemeekins@me.com>
2023-04-12 07:53:44 -0500

    added file factory and __call__

--- app/ProjectClasses/FilePaths.py
+++ app/ProjectClasses/FilePaths.py
@@ -19,15 +19,36 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+#%%
+from typing import Type
+from dataclasses import dataclass
+from dateandtimeclass import DateTimeFormatter
 
+ADJUSTED_FILE_DATE = DateTimeFormatter.shift_start_end_adjust()
+
+@dataclass
 class DefinedFilePaths:
-    def __init__(self) -> None:
-        pass
+    filepath: str
+    filename: str
+    
+    def __str__(self):
+        return f"{self.filepath}{self.filename}"    
+    
+@dataclass
+class FilePathFactory:
+    filepath: Type[DefinedFilePaths]
+    filename: Type[DefinedFilePaths]
 
-    def local_file_path(self) -> dict:
-        local = {
-            "filepath": "/Users/jessemeekins/Documents/VS_CODE/PythonProjectTemplate/app/",
-            "exports": "/Users/jessemeekins/Documents/VS_CODE/PythonProjectTemplate/app/data/exports/"
-        }
+    def __call__(self) -> DefinedFilePaths:
+        return DefinedFilePaths(self.filepath, self.filename).__str__()
 
-        return local
+
+FILE_PATH_FACTORY = {
+    "ALS": FilePathFactory("/Users/jessemeekins/Documents/VS_CODE/PythonProjectTemplate/app/data/exports/",f"ROS11 MFD{ADJUSTED_FILE_DATE}.xml"),
+    "PPL": FilePathFactory("/Users/jessemeekins/Documents/VS_CODE/PythonProjectTemplate/app/data/exports/","person-export-.xml"),
+    "FULL": FilePathFactory("/Users/jessemeekins/Documents/VS_CODE/PythonProjectTemplate/app/data/exports/","Full Roster Export Hourly.xml")
+}
+
+def get_file_path(arg: str) -> DefinedFilePaths:
+    return FILE_PATH_FACTORY[arg]
+

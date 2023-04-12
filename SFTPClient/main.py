113c2c7f1cf5917917ae00f014fc73eabb8ceaae
jessemeekins <jessemeekins@me.com>
2023-04-12 07:56:26 -0500

    added main file

--- SFTPClient/main.py
+++ SFTPClient/main.py
@@ -1,13 +1,8 @@
-import socket
+#%%
+from SFTP_Client import SFTP_MAIN
 
-target = '192.168.1.255'
-port = 6000
+"""
+SFTP_MAIN currently accepts "DEV" and "FULL"
+"""
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-client.connect((target,port))
-client.send(b'GET / HTTP/1.1\r\nHost: google.com\r\n\r\n')
-
-response = client.recv(4096)
-print(response.decode())
-client.close()
+SFTP_MAIN("DEV", False)

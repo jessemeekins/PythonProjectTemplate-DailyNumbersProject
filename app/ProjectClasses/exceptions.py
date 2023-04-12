89a5654a8cf247ef5ed9608dcd1235a59892ae45
jessemeekins <jessemeekins@me.com>
2023-04-12 07:53:22 -0500

    added exceptions classes

---
+++ app/ProjectClasses/exceptions.py
@@ -0,0 +1,11 @@
+
+
+class ImporterError(Exception):
+    """Importer Class Error Handling"""
+
+class MapperError(Exception):
+    """Mapper Class Error Handling"""
+
+class FilePathError(Exception):
+    """Filepath Error Handling"""
+    
